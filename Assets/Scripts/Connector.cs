1a23e2a1419618ae6d0e4a33506a14852401ecfd
Nelson Laracuente <nlaracuente09@gmail.com>
2019-05-31 08:14:32 -0700

    implements basic moving along connections

--- Assets/Scripts/Connector.cs
+++ Assets/Scripts/Connector.cs
@@ -10,6 +10,25 @@ public class Connector : MonoBehaviour
     public delegate void ConnectorClickedEvent(Connector connector);
     public event ConnectorClickedEvent OnClicked;
 
+    [SerializeField]
+    Transform m_anchor;
+
+    /// <summary>
+    /// The transform for the player to anchor to
+    /// </summary>
+    public Transform Anchor
+    {
+        get {
+            // Default to self
+            if (m_anchor == null)
+            {
+                m_anchor = transform;
+            }
+
+            return m_anchor;
+        }
+    }
+
     /// <summary>
     /// Dispatches connector clicked event
     /// </summary>
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,12 @@ public class Player : MonoBehaviour
     float m_moveSpeed = 5f;
 
     /// <summary>
+    /// How long to linger at each connection before moving to the next
+    /// </summary>
+    [SerializeField]
+    float m_connectionDealy = 0.25f;
+
+    /// <summary>
     /// A reference to the line render that shows the connections to the connectors
     /// </summary>
     [SerializeField]
@@ -21,6 +27,10 @@ public class Player : MonoBehaviour
     /// </summary>
     List<Connector> m_connectors;    
 
+    /// <summary>
+    /// True while the player is moving along the connection
+    /// </summary>
+    public bool IsMoving { get; private set; }
 
     /// <summary>
     /// Subscribes to all connectors
@@ -44,6 +54,20 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        // Ignore when moving
+        if (IsMoving)
+        {
+            return;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Move();
+        }
+    }
+
     /// <summary>
     /// Registers the connector as a new connection when it does not exist
     /// When the connector exist it removes everything after it
@@ -51,6 +75,12 @@ public class Player : MonoBehaviour
     /// <param name="connector"></param>
     public void OnConnectorClicked(Connector connector)
     {
+        // Ignore when moving
+        if (IsMoving)
+        {
+            return;
+        }
+
         if (!m_connectors.Contains(connector))
         {
             m_connectors.Add(connector);
@@ -102,11 +132,74 @@ public class Player : MonoBehaviour
     }
 
     /// <summary>
+    /// Updates the position in the line renderer that represent the player's current position
+    /// </summary>
+    void UpdatePlayerPositionInLineRenderer()
+    {
+        if (m_lineRenderer == null)
+        {
+            Debug.LogWarning("WARNING! Player is missing a reference to the line renderer");
+            return;
+        }
+
+        m_lineRenderer.SetPosition(0, transform.position);
+    }
+
+    /// <summary>
     /// Removes all active connections
     /// </summary>
     void OnMouseDown()
     {
+        // Ignore when moving
+        if (IsMoving)
+        {
+            return;
+        }
+
         m_connectors.Clear();
         DrawConnections();
+    }
+
+    /// <summary>
+    /// Triggers the movement routine
+    /// </summary>
+    void Move()
+    {
+        if (!IsMoving)
+        {
+            StartCoroutine(MoveRoutine());
+        }
+    }
+
+    /// <summary>
+    /// Moves the player along all connections
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator MoveRoutine()
+    {
+        IsMoving = true;
+        
+        while (m_connectors.Count > 0)
+        {
+            Connector connector = m_connectors[0];
+            Vector3 destination = connector.Anchor.position;
+
+            while (Vector3.Distance(transform.position, destination) > .001f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, destination, m_moveSpeed * Time.deltaTime);
+                UpdatePlayerPositionInLineRenderer();
+                yield return new WaitForEndOfFrame();
+            }
+
+            transform.position = destination;
+            m_connectors.RemoveAt(0);
+
+            // Re-draw connections to reflect the removed one
+            DrawConnections();
+
+            yield return new WaitForSeconds(m_connectionDealy);
+        }
+
+        IsMoving = false;
     }
 }
