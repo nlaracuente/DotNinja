8edf8a271c62c270cdf1fb6392173c6c60e9551b
Nelson Laracuente <nlaracuente09@gmail.com>
2019-06-02 12:09:01 -0700

    adds simples pause menu and relaces level canvas with a prefab

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -7,13 +7,46 @@ using UnityEngine;
 /// </summary>
 public class MenuController : MonoBehaviour
 {
+    /// <summary>
+    /// A reference to the menu game object
+    /// </summary>
+    [SerializeField]
+    GameObject m_menuGO;
+
+    /// <summary>
+    /// Default menu to closed
+    /// </summary>
+    private void Start()
+    {
+        ToggleMenu(false);
+    }
+
+    /// <summary>
+    /// Triggers the game to start
+    /// </summary>
     public void StartGame()
     {
         GameManager.instance.StartGame();
     }
 
+    /// <summary>
+    /// Triggers the transition to the main menu
+    /// </summary>
     public void MainMenu()
     {
+        // This may be a request from the PAUSE menu which means we need to reset the scale
+        Time.timeScale = 1f;
         GameManager.instance.MainMenu();
+    }
+
+    /// <summary>
+    /// Toggles the menu object to open/close
+    /// </summary>
+    /// <param name="isOpened"></param>
+    public void ToggleMenu(bool isOpened)
+    {
+        if(m_menuGO != null) {
+            m_menuGO.SetActive(isOpened);
+        }
     }
 }
---
+++ Assets/Scripts/MenuButtonIcon.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Toggles the pause menu on/off
+/// </summary>
+[RequireComponent(typeof(Image), typeof(Button))]
+public class MenuButtonIcon : MonoBehaviour
+{
+    /// <summary>
+    /// The sprite that indicates the menu will open when clicked
+    /// </summary>
+    [SerializeField]
+    Sprite m_openMenuSprite;
+
+    /// <summary>
+    /// The sprite that indicates the menu will close when clicked
+    /// </summary>
+    [SerializeField]
+    Sprite m_closeMenuSprite;
+
+    /// <summary>
+    /// True when the menu is opened
+    /// </summary>
+    bool m_isMenuOpened = false;
+
+    /// <summary>
+    /// A reference to the menu controller
+    /// </summary>
+    MenuController m_menuController;
+
+    /// <summary>
+    /// A reference to the UI image
+    /// </summary>
+    Image m_image;
+
+    /// <summary>
+    /// A reference to the UI image
+    /// </summary>
+    Button m_button;
+
+    /// <summary>
+    /// Set references
+    /// </summary>
+    private void Start()
+    {
+        m_image = GetComponent<Image>();
+        m_button = GetComponent<Button>();
+        m_menuController = FindObjectOfType<MenuController>();
+    }
+
+    /// <summary>
+    /// Disables this option while the game is in a state where this cannot be accessed
+    /// </summary>
+    private void Update()
+    {
+        m_button.interactable = GameManager.instance.IsLevelLoaded && 
+                               !GameManager.instance.IsLevelCompleted;
+
+        // Force a menu close as we are in a state where it should not be opened 
+        if (!m_button.interactable && m_isMenuOpened) {
+            OnClicked();
+        }
+    }
+
+    /// <summary>
+    /// Toggles the menu to open/close
+    /// </summary>
+    public void OnClicked()
+    {
+        m_isMenuOpened = !m_isMenuOpened;
+        m_menuController.ToggleMenu(m_isMenuOpened);
+
+        if (m_isMenuOpened) {
+            Time.timeScale = 0f;
+            m_image.sprite = m_closeMenuSprite;
+        } else {
+            Time.timeScale = 1f;
+            m_image.sprite = m_openMenuSprite;
+        }
+    }
+}
