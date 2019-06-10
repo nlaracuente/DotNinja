03c4a2114882679be2307bfd060d9af7499a2f14
Nelson Laracuente <nlaracuente09@gmail.com>
2019-06-10 16:18:40 -0700

    adds snd fx for on button hover

---
+++ Assets/Scripts/Utilities/MouseOverSoundFx.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Triggers the hover sound effect when the mouse enters this object
+/// </summary>
+public class MouseOverSoundFx : MonoBehaviour, IPointerEnterHandler
+{
+    /// <summary>
+    /// For UI buttons
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        PlaySoundFx();
+    }
+
+    /// <summary>
+    /// For non UI object
+    /// </summary>
+    private void OnMouseEnter()
+    {
+        PlaySoundFx();
+    }
+
+    /// <summary>
+    /// Triggers the AudioManager to play hover sound effects
+    /// </summary>
+    void PlaySoundFx()
+    {
+        AudioManager.instance.PlayHoverSound();
+    }
+}
